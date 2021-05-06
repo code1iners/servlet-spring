6e5013f03c46d161115053f81a768ce7f036f897
Codeliner <codeliner96@gmail.com>
2021-05-06 09:11:56 +0900

    Rename v1 front controller

--- src/main/java/hello/servlet/web/frontcontroller/v1/controller/MemberSaveControllerV1.java
+++ src/main/java/hello/servlet/web/frontcontroller/v1/controller/MemberSaveControllerV1.java
@@ -9,19 +9,22 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 
-public class MemberListController implements ControllerV1 {
+public class MemberSaveControllerV1 implements ControllerV1 {
 
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
     @Override
     public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        List<Member> members = memberRepository.findAll();
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
 
-        request.setAttribute("members", members);
+        Member member = new Member(username, age);
+        memberRepository.save(member);
 
-        String viewPath = "/WEB-INF/views/members.jsp";
+        request.setAttribute("member", member);
+
+        String viewPath = "/WEB-INF/views/save-result.jsp";
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request, response);
     }
